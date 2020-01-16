89eb1b909ba140a8213ff93db3388fac3363fe6b
pedago <pedago@pcalb-eb0101.univ-jfc.fr>
2020-01-16 11:40:15 +0100

    TP1 - Cache direct

--- src/cache/Cache.java
+++ src/cache/Cache.java
@@ -5,17 +5,61 @@
  */
 package cache;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import static java.lang.Math.pow;
+import java.util.Scanner;
+
 /**
  *
  * @author pedago
  */
 public class Cache {
 
+    public final int TAILLE_BLOC = 32 ;
+    public static int N = 2;
+    public static int NB_LIGNE = (int) pow(2,N);
+    public static int BITS_DEPLACEMENT = 5 ;
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        int cache[] = new int[NB_LIGNE];
+        boolean Visite[] = new boolean[cache.length];
+        int echecs = 0 ;
+        int succes = 0;
+        try (FileReader fileReader = new FileReader("matrice10.txt");
+                Scanner scanner =new Scanner(fileReader)){
+            while (scanner.hasNext()) {
+                String s = scanner.nextLine();
+                String[] line = s.split(":"); 
+                int value = Integer.parseInt(line[0]);
+                
+                int deplacement = (int) (value % pow(2,BITS_DEPLACEMENT) );
+                int reste = (int) (value / pow(2,BITS_DEPLACEMENT));
+                int numLigne = (int) (reste % pow(2,N));
+                int etiquette = (int) (reste / pow(2,N));
+                //System.out.println(etiquette+" " +numLigne+" "+deplacement);
+                
+                if (Visite[numLigne]==false || cache[numLigne]!=etiquette){
+                    Visite[numLigne]=true;
+                    echecs++;
+                    cache[numLigne]=etiquette;
+                }
+                else{
+                    succes++;  
+                    }
+                }
+            System.out.println("nb echecs = "+echecs);
+            System.out.println("nb succes = "+succes);
+ 
+            }
+        }
     }
     
 }
